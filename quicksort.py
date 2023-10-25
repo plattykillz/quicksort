ca507ca5d4eb997f78ce3f78f2c8ec1c5ffc6f85
Ryan Booth <rboothy23@gmail.com>
2023-10-25 12:44:34 -0400

    first commit

---
+++ quicksort.py
@@ -0,0 +1,16 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quicksort(left) + middle + quicksort(right)
+
+# Example!!!:
+if __name__ == "__main__":
+    unsorted_list = [3, 6, 8, 10, 1, 2, 1, 43, 104, 1, 0, 5, 4, 3, 91]
+    sorted_list = quicksort(unsorted_list)
+    print(sorted_list)
